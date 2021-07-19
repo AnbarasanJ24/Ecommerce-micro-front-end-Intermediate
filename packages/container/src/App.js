d21f8ace42acbb82af6baa62fefb8ba0da205a06
Anbarasan <anbarasanj24@gmail.com>
2021-07-19 07:33:26 +0530

    Added css prefix for conatiner and marketing application

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 import { BrowserRouter } from 'react-router-dom';
+import { createGenerateClassName } from '@material-ui/core';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 export default () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter generateClassName={generateClassName}>
             <div>
                 <Header />
                 <MarketingApp />
