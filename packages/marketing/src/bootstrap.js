9cf27bde806d6224e005066ec54849185d6b6d76
Anbarasan <anbarasanj24@gmail.com>
2021-07-20 08:55:38 +0530

    FInished Routing Module

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 // Mount function to start the app
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) history.listen(onNavigate);
+
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     )
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
+
 }
 
 // If we are in developnment and isolation then call mount immediately 
@@ -16,7 +30,7 @@ if (process.env.NODE_ENV === 'development') {
 
     const devRoot = document.getElementById('_marketing-dev-root');
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
